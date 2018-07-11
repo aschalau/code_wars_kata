e8b4fec34b011e792a9506fecd2a5e13e22ce629
Aaron Schalau <doubledevwp@gmail.com>
2018-07-10 22:51:49 -0400

    changing the folder structure and removing old references

--- GetSum/GetSum_solution.js
+++
@@ -1,9 +0,0 @@
-function GetSum(a, b)
-{
-   var j = a, k = b, sum = 0;
-   if(a > b) k = a, j = b;
-   
-   for(i = j; i <= k; i++) sum += i;
-   
-   return sum;
-}
--- GetSum/GetSum_tests.js
+++
@@ -1,8 +0,0 @@
-Test.describe("Basic Tests:", function(){
-  Test.assertEquals(GetSum(1, 0), 1);
-  Test.assertEquals(GetSum(1, 2), 3);
-  Test.assertEquals(GetSum(0, 1), 1);
-  Test.assertEquals(GetSum(1, 1), 1);
-  Test.assertEquals(GetSum(-1, 0), -1);
-  Test.assertEquals(GetSum(-1, 2), 2); 
-});
