b2cf8e7161ac234be8e23ae2a90e1aa533b104ca
Aaron Schalau <doubledevwp@gmail.com>
2018-07-29 00:43:59 -0400

    all Code Wars Katas are being tested except for one, I need to figure out how to get the test to accept a List<int>

--- C#/tests/CodeWarsKata.Tests.cs
+++ C#/tests/CodeWarsKata.Tests.cs
@@ -1,4 +1,5 @@
-using System;
+// using System;
+// using System.Collections.Generic;
 using Xunit;
 using CodeWarsKata.ClassLib;
 
@@ -63,6 +64,60 @@ namespace tests
         }
 
         [Theory]
+        [InlineData(-1, -1)]
+        [InlineData(5, -5)]
+        [InlineData(0, 0)]
+        public void MakeNegativeTest(int value, int expected)
+        {
+            var result = _cwk.MakeNegative(value);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(5, 6, 30)]
+        [InlineData(45, 50, 2250)]
+        [InlineData(4, 300, 1200)]
+        public void MultiplyTest(int value1, int value2, int expected)
+        {
+            var result = _cwk.Multiply(value1, value2);
+            Assert.Equal(expected, result);
+        }
+
+        // TODO: Figure out how to pass in List<int> or new List<int>
+        //       Two using statemtents are commented out from this as well
+        // [Theory]
+        // [InlineData(List<int> {1, 2, 3, 4, 5}, List<int> {2, 3, 4, 5})]
+        // [InlineData(List<int> {10, -2, 3, 4, 55, -400}, List<int> {10, -2, 3, 4, 55})]
+        // [InlineData(List<int> {2, 2, 1, 2, 1}, List<int> {2, 2, 2, 1})]
+        // [InlineData(List<int> {}, List<int> {})]
+        // public void RemoveSmallestTest(List<int> value, List<int> expected)
+        // {
+        //     var result = _cwk.RemoveSmallest(value);
+        //     Assert.Equal(expected, result);
+        // }
+
+        [Theory]
+        [InlineData(6, "Hello", "HelloHelloHelloHelloHelloHello")]
+        [InlineData(10, "I", "IIIIIIIIII")]
+        public void repeatStrTest(int value1, string value2, string expected)
+        {
+            var result = _cwk.repeatStr(value1, value2);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("Hello World", "olleH dlroW")]
+        [InlineData("I love you", "I evol uoy")]
+        [InlineData("Lucille is so beautiful!", "ellicuL si os !lufituaeb")]
+        public void ReverseWordsTest(string value, string expected)
+        {
+            var result = _cwk.ReverseWords(value);
+            Assert.Equal(expected, result);
+        }
+
+
+        [Theory]
         [InlineData(new int[] {1, 2}, 5)]
         [InlineData(new int[] {2, 3, 4}, 29)]
         [InlineData(new int[] {5, 6, 7}, 110)]
@@ -70,6 +125,15 @@ namespace tests
         public void SquareSumTest(int[] value, int expected)
         {
             var result = _cwk.SquareSum(value);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(1, "One")]
+        [InlineData(5, "Five")]
+        public void SwitchItUpTest(int value, string expected)
+        {
+            var result = _cwk.SwitchItUp(value);
             Assert.Equal(expected, result);
         }
     }
